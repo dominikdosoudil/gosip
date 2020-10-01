1dd8709d2b56ba3a3daaf3bc8252ea4c75e45d1d
Andrei Leontev <andrei.leontev@protonmail.ch>
2020-10-01 07:26:27 +0200

    tag parameter

---
+++ sip/paramutil.go
@@ -0,0 +1,35 @@
+// Copyright 2020 Justine Alexandra Roberts Tunney
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// URI Parameter Util Library
+
+package sip
+
+import (
+	"strings"
+)
+
+const (
+	SipParamTag      = "tag"
+)
+
+func (p *Param) GetTag() string {
+	if p == nil {
+		return ""
+	}
+	if strings.EqualFold(p.Name, SipParamTag) {
+		return p.Value
+	}
+	return p.Next.GetTag()
+}
