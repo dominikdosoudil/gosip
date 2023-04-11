1662dc45a40b167687f2902c5f3abbbc7d815e9a
Dominik Dosoudil <dosoudil@insoft.cz>
2023-04-12 00:52:18 +0200

    remove impl

--- dsp/dsp.go
+++ dsp/dsp.go
@@ -27,56 +27,11 @@ package dsp
 // TODO(jart): How do I make assembly use proper types?
 // func LinearToUlaw(linear int64) (ulaw int64)
 func LinearToUlaw(pcm_val int16) uint8 { // 2's complement (16-bit range)
-
-	var seg int
-	var mask uint8
-	var uval uint8
-
-	// Get the sign and the magnitude of the value.
-	pcm_val = pcm_val >> 2
-	if pcm_val < 0 {
-		pcm_val = -pcm_val
-		mask = 0x7F
-	} else {
-		mask = 0xFF
-	}
-	if pcm_val > _CLIP {
-		pcm_val = _CLIP // clip the magnitude
-	}
-	pcm_val += (_BIAS >> 2)
-
-	/* Convert the scaled magnitude to segment number. */
-	seg = search(pcm_val, seg_uend[:])
-
-	// Combine the sign, segment, quantization bits; and complement the code word.
-	if seg >= 8 {
-		// out of range, return maximum value.
-		uval = uint8(0x7F)
-		return (uval ^ mask)
-	} else {
-		uval = uint8((int16(seg) << 4) | ((pcm_val >> (seg + 1)) & 0xF))
-		return (uval ^ mask)
-	}
+return 
 }
 
 // Turns a μ-Law byte back into an audio sample.
 // func UlawToLinear(ulaw int64) (linear int64)
 func UlawToLinear(u_val uint8) int16 {
-
-	var t int16
-
-	// Complement to obtain normal u-law value.
-	u_val = ^u_val
-
-	// Extract and bias the quantization bits. Then shift up
-	// by the segment number and subtract out the bias.
-
-	t = (int16(u_val&quantMask) << 3) + _BIAS
-	t <<= ((uint(u_val) & segMask) >> segShift)
-
-	if (u_val & signBit) != 0 {
-		return (_BIAS - t)
-	} else {
-		return (t - _BIAS)
-	}
+return
 }
